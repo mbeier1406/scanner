121706bfe052ab2e7b6b732a9804ba9357661cd2
Martin Beier <Martin.Beier@gmx.de>
2024-02-21 20:40:03 +0100

    Jakarta EE Demo-Anwendung

--- src/main/java/com/github/mbeier1406/scanner/Scanner.java
+++ src/main/java/com/github/mbeier1406/scanner/Scanner.java
@@ -1,5 +1,7 @@
 package com.github.mbeier1406.scanner;
 
+import java.util.Arrays;
+
 import jakarta.enterprise.context.RequestScoped;
 import jakarta.inject.Named;
 
@@ -13,16 +15,60 @@ public class Scanner {
 	/** Name des Scanners */
     private String name;
 
+    /** Datum Betriebsbeginn */
+    private String inBetriebSeit;
+
+	/** Interne Nummer */
+    private String nummer;
+
+    /** Wer bei Änderungen benchrichtigt werdne muss */
+    private String[] emailadressen;
+
     public Scanner() {
     }
 
-    public String getName() {
-        return name;
+    public String save() {
+    	System.out.println("Speichern...");
+    	return "index";
     }
 
-    public void setName(String user_name) {
-        this.name = user_name;
-    }
+    public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getInBetriebSeit() {
+		return inBetriebSeit;
+	}
+
+	public void setInBetriebSeit(String inBetriebSeit) {
+		this.inBetriebSeit = inBetriebSeit;
+	}
+
+	public String getNummer() {
+		return nummer;
+	}
+
+	public void setNummer(String nummer) {
+		this.nummer = nummer;
+	}
+
+	public String[] getEmailadressen() {
+		return emailadressen;
+	}
+
+	public void setEmailadressen(String[] emailadressen) {
+		this.emailadressen = emailadressen;
+	}
+
+	@Override
+	public String toString() {
+		return "Scanner [name=" + name + ", inBetriebSeit=" + inBetriebSeit + ", nummer=" + nummer + ", emailadressen="
+				+ Arrays.toString(emailadressen) + "]";
+	}
 
 }
 
